c57a6731b7679fc54cedbc7050cf9497eba11754
Amanda Young <amanda80@ksu.edu>
2019-11-18 15:03:05 -0600

    display, search options

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,44 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            //case 0: no string and ratings to search
+            if (searchString == null && rating.Count == 0) return All;
+
+            List<Movie> results = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                //Case 1: search string and ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if (movie.Title != null 
+                        && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase) 
+                        && rating.Contains(movie.MPAA_Rating))
+                    {
+
+                    }
+                }
+
+                //case 2: search string only
+                else if (searchString !=null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+                //case 3: search ratings only
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating)){
+                        results.Add(movie);
+                    }
+                }
+
+            }
+            return results;
+        }
     }
 }
